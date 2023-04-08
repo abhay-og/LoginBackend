90b8e4e9732d07a7f6e25b88ea6c6e89a6c286eb
abhay-og <102411194+abhay-og@users.noreply.github.com>
2023-04-09 01:50:37 +0530

    LoginIsWorking

---
+++ account/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+from account.models import User
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password2=serializers.CharField(style={'input_type':'password'},write_only=True)
+    class Meta:
+        model=User
+        fields=['email','Date_Of_Birth','Name','TermsAndConditions','ContactNumber','password','password2']
+        extra_kwargs={
+            'password':{'write_only':True}
+        }
+        
+    #validate password and confirm password
+    def validate(self,attrs):
+        password=attrs.get('password')
+        password2=attrs.pop('password2')
+        if password!=password2:
+            raise serializers.ValidationError('Passwords in both the fields do not match')
+        return attrs 
+    
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+    
+    
+class UserLoginSerializer(serializers.ModelSerializer):
+    email=serializers.EmailField(max_length=255)
+    class Meta:
+        model=User
+        fields=['email','password']
--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from account.serializers import *
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from account.renderers import UserRenderer
 
-# Create your views here.
+class UserRegistrationView(APIView):
+    def post(self,request,format=None):
+        serializer=UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user=serializer.save()
+            return Response({'msg':'Registration Successful!'})
+            status=status.HTTP_201_CREATED
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    def post(self,request,format=None):
+        serializer=UserLoginSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            email=serializer.data.get('email')
+            password=serializer.data.get('password')
+            serializer.data['password']=make_password(password)
+            user=authenticate(email=email,password=password)
+            if user is not None:
+                return Response({'msg':'Login Successful!'},status.HTTP_200_OK)
+            else:
+                return Response({'errors':{'non_field_errors':['Email or password is invalid']}},status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
