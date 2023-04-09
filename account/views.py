dd7f9bba2b3b025352fbbc7f6552e876341cc569
abhay-og <102411194+abhay-og@users.noreply.github.com>
2023-04-10 00:17:49 +0530

    ProfileViewWorking

--- account/views.py
+++ account/views.py
@@ -5,13 +5,24 @@ from account.serializers import *
 from django.contrib.auth import authenticate
 from django.contrib.auth.hashers import make_password
 from account.renderers import UserRenderer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+
+def get_tokens_for_user(user): #generates tokens manually
+    refresh = RefreshToken.for_user(user)
+
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
 
 class UserRegistrationView(APIView):
     def post(self,request,format=None):
         serializer=UserRegistrationSerializer(data=request.data)
         if serializer.is_valid(raise_exception=True):
             user=serializer.save()
-            return Response({'msg':'Registration Successful!'})
+            token= get_tokens_for_user(user)
+            return Response({'token':token,'msg':'Registration Successful!'})
             status=status.HTTP_201_CREATED
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
@@ -25,7 +36,15 @@ class UserLoginView(APIView):
             serializer.data['password']=make_password(password)
             user=authenticate(email=email,password=password)
             if user is not None:
-                return Response({'msg':'Login Successful!'},status.HTTP_200_OK)
+                token= get_tokens_for_user(user)
+                return Response({'token':token,'msg':'Login Successful!'},status.HTTP_200_OK)
             else:
                 return Response({'errors':{'non_field_errors':['Email or password is invalid']}},status.HTTP_404_NOT_FOUND)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class UserProfileView(APIView):
+    renderer_classes=[UserRenderer]
+    permission_classes=[IsAuthenticated]
+    def get(self,request,format=None):
+        serializer=UserProfileSerializer(request.user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
