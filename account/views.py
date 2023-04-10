50882263e30e74e5cf936f0b479287438544e7cc
abhay-og <102411194+abhay-og@users.noreply.github.com>
2023-04-10 11:34:43 +0530

    BackendDone

--- account/views.py
+++ account/views.py
@@ -48,3 +48,26 @@ class UserProfileView(APIView):
     def get(self,request,format=None):
         serializer=UserProfileSerializer(request.user)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class UserChangePasswordView(APIView):
+    renderer_classes=[UserRenderer]
+    permission_classes=[IsAuthenticated]
+    def post(self,request,format=None):
+        serializer=UserChangePasswordSerializer(data=request.data, context={'user':request.user})
+        if serializer.is_valid():
+            return Response({'msg':'Password changes successfully!'},status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+class SendPasswordResetEmailView(APIView):
+    renderer_classes = [UserRenderer]
+    def post(self, request, format=None):
+        serializer = SendPasswordResetEmailSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response({'msg':'Password Reset link send. Please check your Email'}, status=status.HTTP_200_OK)
+
+class UserPasswordResetView(APIView):
+  renderer_classes = [UserRenderer]
+  def post(self, request, uid, token, format=None):
+    serializer = UserPasswordResetSerializer(data=request.data, context={'uid':uid, 'token':token})
+    serializer.is_valid(raise_exception=True)
+    return Response({'msg':'Password Reset Successfully'}, status=status.HTTP_200_OK)
